c5bf7fa3c491ec77a3ba36b1e9c013b5b4516880
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 11:52:16 -0600

    add validations with annotations on my ToDo dto

--- Backend/src/main/java/com/encora/todolist/dto/ToDoDTO.java
+++ Backend/src/main/java/com/encora/todolist/dto/ToDoDTO.java
@@ -1,16 +1,24 @@
 package com.encora.todolist.dto;
 
 import java.time.LocalDate;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class ToDoDTO {
+    @NotBlank(message = "Name of the task is required")
+    @Size(max = 120, message = "The max size for the name is 120 character")
     private String text;
+    @FutureOrPresent(message = "Due date cannot be in the past")
     private LocalDate dueDate;
-    private Boolean done;
     private String priority;
 }
