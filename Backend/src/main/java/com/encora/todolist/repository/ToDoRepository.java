63048cb628051fd10222507a6d0b9bc89b4dd1f2
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 10:37:41 -0600

    update repository deleting all related to jpa

--- Backend/src/main/java/com/encora/todolist/repository/ToDoRepository.java
+++ Backend/src/main/java/com/encora/todolist/repository/ToDoRepository.java
@@ -1,10 +1,7 @@
 package com.encora.todolist.repository;
 
-import com.encora.todolist.model.ToDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ToDoRepository extends JpaRepository<ToDo, Long>, JpaSpecificationExecutor<ToDo>{
+public interface ToDoRepository {
 }
