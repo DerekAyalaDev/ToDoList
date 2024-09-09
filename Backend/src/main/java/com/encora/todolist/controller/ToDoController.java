667f48f21c163a79832db60ae92b1b51d6c330de
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 12:08:53 -0600

    add @Valid for annotations validations

--- Backend/src/main/java/com/encora/todolist/controller/ToDoController.java
+++ Backend/src/main/java/com/encora/todolist/controller/ToDoController.java
@@ -5,6 +5,8 @@ import com.encora.todolist.dto.ToDoDTO;
 import com.encora.todolist.model.ToDo;
 import com.encora.todolist.service.ToDoService;
 import java.util.List;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +18,17 @@ public class ToDoController {
     private final ToDoService tds;
     
     @GetMapping("/todos")
-    public ResponseEntity<List<ToDo>> getToDos(@RequestBody SearchDTO dto) {
+    public ResponseEntity<List<ToDo>> getToDos(@Valid @RequestBody SearchDTO dto) {
         return tds.searchToDos(dto);
     }
     
     @PostMapping("/todos")
-    public ResponseEntity<String> createToDo(@RequestBody ToDoDTO dto) {
+    public ResponseEntity<String> createToDo(@Valid @RequestBody ToDoDTO dto) {
         return tds.createToDo(dto);
     }
     
     @PutMapping("todos/{id}")
-    public ResponseEntity<String> updateToDo(@PathVariable("id") Long id, @RequestBody ToDoDTO dto) {
+    public ResponseEntity<String> updateToDo(@PathVariable("id") Long id, @Valid @RequestBody ToDoDTO dto) {
         return tds.updateToDo(id, dto);
     }
 
@@ -39,5 +41,4 @@ public class ToDoController {
     public ResponseEntity<String> updateToDoStatus(@PathVariable("id") Long id) {
         return tds.updateDone(id);
     }
-
 }
