32bcabe280c17d437707e42688c33988424d3889
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 10:28:51 -0600

    create all the endpoints for: creation, update, delete and search

--- Backend/src/main/java/com/encora/todolist/controller/ToDoController.java
+++ Backend/src/main/java/com/encora/todolist/controller/ToDoController.java
@@ -1,13 +1,43 @@
 package com.encora.todolist.controller;
 
+import com.encora.todolist.dto.SearchDTO;
+import com.encora.todolist.dto.ToDoDTO;
+import com.encora.todolist.model.ToDo;
 import com.encora.todolist.service.ToDoService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ToDoController {
     private final ToDoService tds;
+    
+    @GetMapping("/todos")
+    public ResponseEntity<List<ToDo>> getToDos(@RequestBody SearchDTO dto) {
+        return tds.searchToDos(dto);
+    }
+    
+    @PostMapping("/todos")
+    public ResponseEntity<String> createToDo(@RequestBody ToDoDTO dto) {
+        return tds.createToDo(dto);
+    }
+    
+    @PutMapping("todos/{id}")
+    public ResponseEntity<String> updateToDo(@PathVariable("id") Long id, @RequestBody ToDoDTO dto) {
+        return tds.updateToDo(id, dto);
+    }
+
+    @DeleteMapping("todos/{id}")
+    public ResponseEntity<String> deleteToDo(@PathVariable("id") Long id) {
+        return tds.deleteToDo(id);
+    }
+
+    @PutMapping("todos/{id}/status")
+    public ResponseEntity<String> updateToDoStatus(@PathVariable("id") Long id) {
+        return tds.updateDone(id);
+    }
+
 }
