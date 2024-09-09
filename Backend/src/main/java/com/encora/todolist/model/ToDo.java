efa1e4fbdd56edce97d242d5f4655562b0cf1433
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 10:37:22 -0600

    update model deleting all things related to jpa

--- Backend/src/main/java/com/encora/todolist/model/ToDo.java
+++ Backend/src/main/java/com/encora/todolist/model/ToDo.java
@@ -1,46 +1,19 @@
 package com.encora.todolist.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "to_dos")
 public class ToDo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
-    @Column(name = "text", nullable = false, length=120)
-    @NonNull
     private String text;
-    
-    @Column(name = "due_date", nullable = true)
     private LocalDate dueDate;
-    
-    @Column(name = "done", nullable = false)
-    @NonNull
     private Boolean done;
-    
-    @Column(name = "done_date", nullable = true)
     private LocalDateTime doneDate;
-    
-    @Column(name = "priority", nullable = false)
-    @NonNull
     private String priority;
-    
-    @Column(name = "creation_date", nullable = false)
-    @NonNull
     private LocalDateTime creationDate;
 
     @Override
