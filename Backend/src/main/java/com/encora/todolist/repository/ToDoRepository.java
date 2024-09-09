f05780ecd91d44efbae7a64146ece6d731606bb0
DerekAyalaDev <derek.ayala@encora.com>
2024-09-09 11:51:04 -0600

    update ToDoRepository to works only as an interface

--- Backend/src/main/java/com/encora/todolist/repository/ToDoRepository.java
+++ Backend/src/main/java/com/encora/todolist/repository/ToDoRepository.java
@@ -1,7 +1,15 @@
 package com.encora.todolist.repository;
 
-import org.springframework.stereotype.Repository;
+import com.encora.todolist.dto.SearchDTO;
+import com.encora.todolist.model.ToDo;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface ToDoRepository {
+    List<ToDo> findAll();
+    Optional<ToDo> findById(Long id);
+    void save(ToDo toDo);
+    void delete(ToDo toDo);
+    List<ToDo> findByCriteria(SearchDTO searchDTO);
 }
