1962173696e28d974369a0a1b3db600a2d268695
DerekAyalaDev <derek.ayala@encora.com>
2024-09-10 20:57:07 -0600

    create Metrics part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import { SearchForm } from './components/SearchForm';
 import { Container } from './components/Container';
+import './styles/ContainerStyles.css';
+import './styles/SearchFormStyles.css';
+import './styles/MetricsStyles.css';
+import { Metrics } from './components/Metrics';
+
 function App() {
   return (
     <Container>
       <SearchForm />
+      <Metrics />
     </Container>
   );
 }
---
+++ frontend/src/components/Metrics.tsx
@@ -0,0 +1,28 @@
+export const Metrics = () => {
+  return (
+    <div className="container-item">
+      <h4>Average time to finish tasks</h4>
+      <div className="grid-two-column">
+        <div className="metrics-container">
+          <p className="tittle">Average time</p>
+          <span>00:00:00</span>
+          <p>HH:MM:SS</p>
+        </div>
+        <div className="metrics-container">
+          <p className="tittle">Average time by priority</p>
+          <div>
+            <p>
+              Low: <span>00:00:00</span>
+            </p>
+            <p>
+              Medium: <span>00:00:00</span>
+            </p>
+            <p>
+              High: <span>00:00:00</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
