64232788ba5bcfcb2aa93b8cea9b0abce304f5f2
DerekAyalaDev <derek.ayala@encora.com>
2024-09-11 13:29:03 -0600

    use the new custom form on SearchForm

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,5 +1,5 @@
 import { SearchSelect } from "./SearchSelect";
-
+import { CustomForm } from "./CustomForm";
 export const SearchForm = () => {
   const priorityOptions = [
     { value: "", label: "All" },
@@ -15,17 +15,7 @@ export const SearchForm = () => {
   ];
   return (
     <div className="container-item container-border border-dotted">
-      <div className="container-field">
-        <label htmlFor="name">Name</label>
-        <input className="form-input" type="text" id="name" />
-      </div>
-      <SearchSelect id="priority" label="Priority" options={priorityOptions} />
-      <div className="row-button">
-        <SearchSelect id="state" label="State" options={stateOptions} />
-        <button className="form-btn" type="submit">
-          Search
-        </button>
-      </div>
-    </form>
+      <CustomForm btnLabel="Search" priorityOptions={priorityOptions} stateOptions={stateOptions} />
+    </div>
   );
 };
