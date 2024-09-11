e5cc1b6904c25331fa9dd5830ef3ce6134209ad0
DerekAyalaDev <derek.ayala@encora.com>
2024-09-11 13:23:43 -0600

    update container styles with borders

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -2,7 +2,7 @@ import { ContainerProps } from "../types/ContainerTypes"
 import '../styles/ContainerStyles.css'
 export const Container = (props: ContainerProps) => {
   return (
-    <div className="container">
+    <div className="container container-border border-solid">
       {props.children}
     </div>
   )
