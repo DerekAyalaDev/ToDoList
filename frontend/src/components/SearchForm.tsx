88b029f896c2f313ed9b80e2a734252cd5f405cf
DerekAyalaDev <derek.ayala@encora.com>
2024-09-10 20:56:33 -0600

    move styles between files where they belong

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,29 +1,31 @@
-import { SearchSelect } from './SearchSelect';
-import '../styles/SearchFormStyles.css';
+import { SearchSelect } from "./SearchSelect";
+
 export const SearchForm = () => {
   const priorityOptions = [
-    { value: 'All', label: 'All' },
-    { value: 'Low', label: 'Low' },
-    { value: 'Medium', label: 'Medium' },
-    { value: 'High', label: 'High' },
+    { value: "", label: "All" },
+    { value: "Low", label: "Low" },
+    { value: "Medium", label: "Medium" },
+    { value: "High", label: "High" },
   ];
 
   const stateOptions = [
-    { value: 'All', label: 'All' },
-    { value: 'true', label: 'Done' },
-    { value: 'false', label: 'Undone' },
+    { value: "", label: "All" },
+    { value: "true", label: "Done" },
+    { value: "false", label: "Undone" },
   ];
   return (
-    <form className='form'>
-      <div className='form-field'>
+    <form className="container-item ">
+      <div className="container-field">
         <label htmlFor="name">Name</label>
-        <input className='form-input' type="text" id="name" />
+        <input className="form-input" type="text" id="name" />
       </div>
-      <SearchSelect id="priority" label="Priority" options={priorityOptions}/>
-      <div className='row-button'>
-        <SearchSelect id="state" label='State' options={stateOptions}/>
-        <button className='form-btn' type="submit">Search</button>
+      <SearchSelect id="priority" label="Priority" options={priorityOptions} />
+      <div className="row-button">
+        <SearchSelect id="state" label="State" options={stateOptions} />
+        <button className="form-btn" type="submit">
+          Search
+        </button>
       </div>
     </form>
   );
-}
+};
--- frontend/src/components/SearchSelect.tsx
+++ frontend/src/components/SearchSelect.tsx
@@ -2,7 +2,7 @@ import { SearchSelectProps } from "../types/SearchSelectTypes"
 
 export const SearchSelect = (props: SearchSelectProps) => {
   return (
-    <div className="form-select-group form-field">
+    <div className="form-select-group container-field">
       <label className="text" htmlFor={props.id}>{props.label}</label>
       <select className="form-select" id={props.id}>
         {props.options.map((option) => (
