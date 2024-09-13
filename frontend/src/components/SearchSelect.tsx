58834bdd959b00ae9f0a6fe91042052981270762
DerekAyalaDev <derek.ayala@encora.com>
2024-09-13 17:57:45 -0600

    update search select with the new props

--- frontend/src/components/SearchSelect.tsx
+++ frontend/src/components/SearchSelect.tsx
@@ -1,17 +1,25 @@
-import React from "react"
-import { SearchSelectProps } from "../types/searchSelectProps.types"
+import React from "react";
+import { SearchSelectProps } from "../types/searchSelectProps.types";
 
-export const SearchSelect: React.FC<SearchSelectProps> = ({id, label, options}) => {
+export const SearchSelect: React.FC<SearchSelectProps> = ({
+  id,
+  label,
+  options,
+  value,
+  onChange,
+}) => {
   return (
     <div className="form-select-group container-field">
-      <label className="text" htmlFor={id}>{label}</label>
-      <select className="form-select" id={id}>
-        {options.map((option) => (
+      <label className="text" htmlFor={id}>
+        {label}
+      </label>
+      <select className="form-select" id={id} value={value} onChange={onChange}>
+        {options?.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
       </select>
     </div>
-  )
-}
+  );
+};
