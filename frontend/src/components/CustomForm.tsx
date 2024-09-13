fb8dff6f5a5059138d3352d70be1e8ed350632c4
DerekAyalaDev <derek.ayala@encora.com>
2024-09-13 17:59:08 -0600

    update CustomForm with to use the values from the ToDo to edit

--- frontend/src/components/CustomForm.tsx
+++ frontend/src/components/CustomForm.tsx
@@ -1,22 +1,57 @@
 import { SearchSelect } from "./SearchSelect";
 import { CustomFormProps } from "../types/customFormProps.types";
 import { DateInput } from "./DateInput";
+import { useEffect, useState } from "react";
 
-export const CustomForm = ({btnLabel, priorityOptions, stateOptions, includeDate}: CustomFormProps) => {
+export const CustomForm = ({
+  btnLabel,
+  priorityOptions,
+  stateOptions,
+  includeDate,
+  initialValues,
+}: CustomFormProps) => {
+  const [name, setName] = useState(initialValues?.name || "");
+  const [priority, setPriority] = useState(
+    initialValues?.priority || priorityOptions[0].value
+  );
+  const [state, setState] = useState(stateOptions?.[0].value);
+  const [dueDate, setDueDate] = useState(initialValues?.dueDate || "");
+
+  useEffect(() => {
+    setName(initialValues?.name || "");
+    setPriority(initialValues?.priority || priorityOptions[0].value);
+    setDueDate(initialValues?.dueDate || "");
+  }, [initialValues]);
+
   return (
     <form className="form">
       <div className="container-field">
         <label htmlFor="name">Name</label>
-        <input className="form-input" type="text" id="name" />
+        <input
+          className="form-input"
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
-      <SearchSelect id="priority" label="Priority" options={priorityOptions} />
-      {includeDate && <DateInput id="dueDate" label="Due Date" />}
+      <SearchSelect
+        id="priority"
+        label="Priority"
+        options={priorityOptions}
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+      />
       <div className="row-button">
-        <SearchSelect id="state" label="State" options={stateOptions} />
+        {includeDate ? (
+          <DateInput id="dueDate" label="Due Date" value={dueDate} onChange={(e) => setDueDate(e.target.value)}/>
+        ) : (
+          <SearchSelect id="state" label="State" options={stateOptions} value={state} onChange={(e) => setState(e.target.value)}/>
+        )}
         <button className="form-btn background-green" type="submit">
           {btnLabel}
         </button>
       </div>
     </form>
-  )
-}
+  );
+};
