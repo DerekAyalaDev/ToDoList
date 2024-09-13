2d78f24b19c767f5019e8b0ac2975d6b8e47f314
DerekAyalaDev <derek.ayala@encora.com>
2024-09-13 17:58:17 -0600

    update customFormProps to be able to do Edits to

--- frontend/src/types/customFormProps.types.ts
+++ frontend/src/types/customFormProps.types.ts
@@ -1,9 +1,10 @@
 import { Option } from "./searchSelectProps.types"
-
+import { ToDo } from "./toDoTableProps.type"
 export type CustomFormProps = {
   btnLabel: string,
   btnOnClick?: () => void,
   priorityOptions: Option[],
-  stateOptions: Option[],
+  stateOptions?: Option[],
   includeDate?: boolean,
+  initialValues?: ToDo,
 }
