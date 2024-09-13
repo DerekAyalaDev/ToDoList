611b57739683434e19db3360c5a1add6d7df8b64
DerekAyalaDev <derek.ayala@encora.com>
2024-09-13 17:59:37 -0600

    update DateInput with the value and onChange

--- frontend/src/components/DateInput.tsx
+++ frontend/src/components/DateInput.tsx
@@ -1,10 +1,16 @@
 import { DateInputProps } from "../types/dateInputProps.types";
 
-export const DateInput = ({ id, label }: DateInputProps) => {
+export const DateInput = ({ id, label, value, onChange }: DateInputProps) => {
   return (
     <div className="form-select-group container-field">
       <label htmlFor={id}>{label}</label>
-      <input className="form-input" type="date" id={id} />
+      <input
+        className="form-input"
+        type="date"
+        id={id}
+        value={value}
+        onChange={onChange}
+      />
     </div>
-  )
-}
+  );
+};
