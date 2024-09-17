2a16335470f05d2fdfe0f97e887b6ced3cdc8f14
DerekAyalaDev <derek.ayala@encora.com>
2024-09-17 12:28:08 -0600

    create function to handle search submit

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,10 +1,32 @@
 import { CustomForm } from "./CustomForm";
 import { priorityOptions, stateOptions } from "../utils/options";
+import { useSearchContext } from "./SearchContext";
 
 export const SearchForm = () => {
+  const { setSearchState, searchState } = useSearchContext();
+
+  const handleSearchSubmit = (values: {
+    name: string;
+    priority: string;
+    state: string;
+  }) => {
+    // Actualizar el estado global con los nuevos valores de búsqueda
+    setSearchState((prevState) => ({
+      ...prevState,
+      text: values.name,
+      priority: values.priority,
+      state: values.state, // Actualizamos el estado basado en los valores "true", "false" o ""
+    }));
+  };
+
   return (
     <div className="container-item padding-vertical-20 container-border border-dotted">
-      <CustomForm btnLabel="Search" priorityOptions={priorityOptions} stateOptions={stateOptions} />
+      <CustomForm
+        btnLabel="Search"
+        priorityOptions={priorityOptions}
+        stateOptions={stateOptions}
+        onSearchSubmit={handleSearchSubmit}
+      />
     </div>
   );
 };
