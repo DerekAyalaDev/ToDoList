2b4acad09dce50e28d3fd10069dea45d9c3d3717
DerekAyalaDev <derek.ayala@encora.com>
2024-09-17 16:35:21 -0600

    update pagination to allow horizontal scroll after 5 pages

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -7,28 +7,8 @@ interface PaginationProps {
 
 export const Pagination = ({ totalPages }: PaginationProps) => {
   const { searchState, setSearchState } = useSearchContext();
-  const [pages, setPages] = useState<number[]>([]);
   const currentPage = searchState.pageNumber || 0;
 
-  // Función para generar las páginas y centrar la actual
-  const generatePages = (total: number) => {
-    const visiblePages = 5; // Mostrar 5 páginas visibles a la vez
-    const startPage = Math.max(0, currentPage - Math.floor(visiblePages / 2));
-    const endPage = Math.min(total - 1, startPage + visiblePages - 1);
-
-    const pagesArray = [];
-    for (let i = startPage; i <= endPage; i++) {
-      pagesArray.push(i);
-    }
-    setPages(pagesArray);
-  };
-
-  // Llamar a generatePages cuando totalPages o currentPage cambian
-  useEffect(() => {
-    generatePages(totalPages);
-  }, [totalPages, currentPage]);
-
-  // Función para manejar el cambio de página
   const handlePageChange = (page: number) => {
     setSearchState((prevState) => ({
       ...prevState,
@@ -36,10 +16,12 @@ export const Pagination = ({ totalPages }: PaginationProps) => {
     }));
   };
 
+  const pagesArray = Array.from({ length: totalPages }, (_, i) => i);
+
   return (
     <div className="pagination-container">
       <ul className="pagination-list">
-        {pages.map((page) => (
+        {pagesArray.map((page) => (
           <li
             key={page}
             className={`pagination-item ${page === currentPage ? "active" : ""}`}
