5c72e84e3806963bda99060fb512d8f25902724f
DerekAyalaDev <derek.ayala@encora.com>
2024-09-16 21:14:11 -0600

    update Todo properties and delete TodoTableProps

--- frontend/src/types/toDoTableProps.type.ts
+++ frontend/src/types/toDoTableProps.type.ts
@@ -1,11 +1,7 @@
 export type ToDo = {
   id: number,
   done: boolean
-  name: string,
+  text: string,
   priority: string,
   dueDate: string | null,
-}
-
-export type TodoTableProps = {
-  todos: ToDo[],
 }
