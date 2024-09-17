5a49ee0a2c2d5e6b5b74d591975f377b161fde84
DerekAyalaDev <derek.ayala@encora.com>
2024-09-17 12:26:59 -0600

    add the new type and the function to update search values

--- frontend/src/types/customFormProps.types.ts
+++ frontend/src/types/customFormProps.types.ts
@@ -1,10 +1,12 @@
 import { Option } from "./selectInputProps.types"
 import { ToDo } from "./toDoTableProps.type"
+import { searchFormType } from "./searchForm.types"
 export type CustomFormProps = {
   btnLabel: string,
-  btnOnClick?: () => void,
   priorityOptions: Option[],
   stateOptions?: Option[],
   includeDate?: boolean,
   initialValues?: ToDo,
+  searchFormValues?: searchFormType,
+  onSearchSubmit?: (values: { name: string; priority: string; state: string }) => void
 }
